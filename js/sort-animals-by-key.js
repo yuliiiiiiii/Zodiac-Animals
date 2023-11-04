e92ffed5204c4f507abd82f61cb8b0b7112976d4
Brian McClain <brianmccln@yahoo.com>
2023-11-04 16:28:11 -0400

    improve comments

--- js/sort-animals-by-key.js
+++ js/sort-animals-by-key.js
@@ -19,29 +19,29 @@ function renderAnimals() { // output the 12 animals to the section
         const animalImg = new Image(); // make an image
         animalImg.src = `images/animals/${animal.eng}.jpg`; // concat path to jpg
         animalImg.className = 'animal-pic'; // assign class to animal img
-        divvy.appendChild(animalImg); // output the image inside the div:
+        divvy.appendChild(animalImg); // output the animal image to the div
         const charImg = new Image(); // make image to hold Chinese character
-        charImg.src = `images/chars/char-${animal.chr}.jpg`; // concat path to char jpg:
+        charImg.src = `images/chars/char-${animal.chr}.jpg`; // concat path to char jpg
         charImg.className = 'chinese-char'; // assign class to character img
-        divvy.appendChild(charImg);
+        divvy.appendChild(charImg); // output the character image to the div
         const engSpan = document.createElement('span'); // make a span tag
         engSpan.className = 'english'; // assign class to span tag
-        engSpan.textContent = animal.eng; // output English name to span tag
+        engSpan.textContent = animal.eng; // output English name text to span tag
         divvy.appendChild(engSpan); // output span tag to divvy
         // make a span tag to hold Pinyin name of animal
         const pinSpan = document.createElement('span'); // make a span tag
         pinSpan.className = 'pinyin'; // assign class to span tag
-        pinSpan.innerHTML = animal.pin; // output Pinyin to span tag (contains HTML entitie, so use innerHTML()
-        divvy.appendChild(pinSpan); // output Pin name to divvy
+        pinSpan.innerHTML = animal.pin; // output Pinyin text to span (HTML entities require innerHTML)
+        divvy.appendChild(pinSpan); // output Pinyin span name to divvy
         // make a p tag to hold a string of animal years in 12 year intervals
-        const yearsP = document.createElement('p');
-        yearsP.className = 'zodiac-year'; // assign class to p-tag
+        const yearsP = document.createElement('p'); // make a p tag
+        yearsP.className = 'zodiac-year'; // assign class to p tag
         divvy.appendChild(yearsP); // append p tag to divvy
         // run a loop that starts w current year, and concats
         // years backwards in time, in increments of 12 years 
-        let yearsStr = animal.yr + " "; // start a string of years, beginning w current year
-        for(let y = 12; y <= 168; y += 12) { // run a loop from 12-168 in increments of 12
-            yearsStr += (animal.yr - y) + " "; // concat the next year in 12-year cycle
+        let yearsStr = animal.yr + " "; // string of years begins w current year
+        for(let y = 12; y <= 168; y += 12) { // loop from 12-168 in increments of 12
+            yearsStr += (animal.yr - y) + " "; // concat next year in the cycle
         }
         yearsP.textContent = yearsStr; // output animal yr string to p-tag
     }); // end forEach()
@@ -52,11 +52,11 @@ renderAnimals(); // render animals on page load; default order is by Eng, asc
 // sortAnimals() runs on change to select menu:
 function sortAnimals() {
     section.innerHTML = ""; // clear section to make way for fresh output
-    let k = menu.value; // get menu value, which is sort key "eng", "chi" or "yr"
-    // if sort key is "yr" (year), do a number sort
-    // if sort key is "eng" or "chi", do a string sort
-    // use dynamic obj key accessor syntex: [k], where k is either "eng" or "chi":
+    let k = menu.value; // get sort menu value: "eng", "chi" or "yr"
+    // if sort key is "yr" (year), do a number sort in ascending order
+    // if sort key is "eng" or "chi", do a string sort in ascending order
+    // use dynamic obj key accessor, where a[k] is either "eng" or "chi":
     k == "yr" ? arr.sort((a,b) => a.yr-b.yr) : arr.sort((a,b) => a[k]>b[k] ? 1 : -1);
     if(descCB.checked) animals.reverse(); // if Descending is checked; reverse sort
-    renderAnimals(); // re-render the newly sorted animals
+    renderAnimals(); // re-render the sorted animals
 }
