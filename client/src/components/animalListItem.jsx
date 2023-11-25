20d868b590407e6fa1a0cb57501d930dd121d894
Yuli Wang <yuliwang1007@gmail.com>
2023-11-24 17:29:55 -0700

    show animal names

--- client/src/components/animalListItem.jsx
+++ client/src/components/animalListItem.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../styles/animalListItem.scss";
 
 const AnimalListItem = (props) => {
-  const { eng, chr, chi, pin, yr, partners, traits, index} = props;
+  const { eng, chr, chi, pin, yr, partners, traits} = props.animal;
+  const {index} = props.index
 
   return (
     <div>
-      <img src={`/images/animals/${eng}.jpg`} />
+      {/* <img src={`/images/animals/${eng}.jpg`} /> */}
       {/* the url is not correct form! */}
+      <span>
+        {eng}
+      </span>
     </div>
   )
 }
