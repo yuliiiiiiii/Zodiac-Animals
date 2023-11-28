90791b0dadc8265d4d08b30ea8b865dce533f91b
Yuli Wang <yuliwang1007@gmail.com>
2023-11-27 18:34:58 -0700

    show pinyin

--- client/src/components/animalListItem.jsx
+++ client/src/components/animalListItem.jsx
@@ -4,13 +4,19 @@ import "../styles/animalListItem.scss";
 const AnimalListItem = (props) => {
   const { eng, chr, chi, pin, yr, partners, traits} = props.animal;
   const {index} = props.index
+  
+  const formatPinYin = () => {
+    return <span className="pinyin" dangerouslySetInnerHTML={{ __html: pin }}></span>
+  }
 
   return (
     <div>
-      <img src={`/images/animals/${eng}.jpg`} />
-      <span>
+      <img src={`/images/animals/${eng}.jpg`} className="animal-pic"/>
+      <img src={`/images/chars/char-${chr}.jpg`} className="chinese-char"/>
+      <span className="english">
         {eng}
       </span>
+      {formatPinYin()}
     </div>
   )
 }
