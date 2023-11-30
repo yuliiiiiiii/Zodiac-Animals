152cbd282c6af3b58a639fe5fb227f7862f5aede
Yuli Wang <yuliwang1007@gmail.com>
2023-11-30 16:19:34 -0700

    style animalInfo

--- client/src/components/animalInfo.jsx
+++ client/src/components/animalInfo.jsx
@@ -5,9 +5,10 @@ const AnimalInfo = (props) => {
 const { eng, partners, traits} = props;
 
   return (
-    <div>
-      <p></p>
-
+    <div className="divvy animal-info" >
+      <p className="animal-name">{eng}</p>
+      <p>Partners: {partners}</p>
+      <p>Characteristics: {traits}</p>
     </div>
   )
   
