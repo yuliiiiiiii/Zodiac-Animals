209892d90c9c29c6c90b198e02474e9ca56abe20
Yuli Wang <yuliwang1007@gmail.com>
2023-12-12 18:36:05 -0700

    npm init and create app.js

---
+++ server/app.js
@@ -0,0 +1,19 @@
+//declarations
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const PORT = 8080;
+const enviroment = 'dev';
+
+const app = express();
+
+//middleware setup
+app.use(morgan(enviroment));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({greetings: 'hello world'});
+})
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
