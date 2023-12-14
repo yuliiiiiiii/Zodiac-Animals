8952f616571223347e369fd03d2b946f7ce5a266
Yuli Wang <yuliwang1007@gmail.com>
2023-12-13 19:07:21 -0700

    add seed

---
+++ server/db/seeds/animals.sql
@@ -0,0 +1,4 @@
+INSERT INTO animals (eng, chr, chi, pin, yr, partners, traits) 
+  VALUES 
+    ('dog', 'gou', 'gou', 'gou&#780;', 2030, 'Tiger and Horse', 'Loyal, sociable, courageous, diligent, steady, lively, adaptable, smart.' );
+
