897cac842d4d5ad9a8001e833aba8f8e749b5ee5
Yuli Wang <yuliwang1007@gmail.com>
2023-12-21 17:03:10 -0500

    create stories table

---
+++ server/db/seeds/stories.sql
@@ -0,0 +1,14 @@
+INSERT INTO stories (animal_id, story) 
+  VALUES 
+(1, 'No story yet'),
+(2, 'No story yet'),
+(3, 'No story yet'),
+(4, 'No story yet'),
+(5, 'No story yet'),
+(6, 'No story yet'),
+(7, 'No story yet'),
+(8, 'No story yet'),
+(9, 'No story yet'),
+(10, 'No story yet'),
+(11, 'No story yet'),
+(12, 'No story yet')
