ad531dbdf6fdc1e4539fe15d7fd1084d99ea0450
Yuli Wang <yuliwang1007@gmail.com>
2023-12-22 18:33:48 -0500

    add comment

--- client/src/providers/SelectProvider.jsx
+++ client/src/providers/SelectProvider.jsx
@@ -23,6 +23,8 @@ export default function SelectProvider(props) {
   const handleCheck = (eventCheck) => {
     if (eventCheck.target.checked) {
       setCheck(true);
+      
+      // There is something wrong here, when there is a re-render, it got triggered
       setSortedAnimals(sortedAnimals.reverse());
     } else {
       setCheck(false);
