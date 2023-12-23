b7c5ff984ee5fbd3926f8e7651e6a178c06b0840
Yuli Wang <yuliwang1007@gmail.com>
2023-12-22 19:15:32 -0500

    add comment

--- client/src/components/aiStory.jsx
+++ client/src/components/aiStory.jsx
@@ -8,7 +8,7 @@ const AiStory = () => {
   
   const [animalId, setAnimalId] = useState(1);
 
-
+// probably needs to create a new state to control the sequence of select option animal's order(by year), and makes it static so that sort doesn't affect option animal's order!!!
   const optionArray = sortedAnimals.map(animal => {
     return <option key={animal.id} value={animal.id} >{animal.eng}</option>
   })
